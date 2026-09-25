028408fb3cfd463499df422c293351baf9724459
Fariz <rustamfariz022.gmail.com>
2026-09-25 20:09:17 +0800

    menambahkan tipe data dan while loop

---
+++ Looping(for).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//penggunaan for loop dan if else pada contoh nyata//
+
+int main(){
+    int angka_awal;
+    int angka_akhir;
+    cout<<"Masukkan angka pertama yang ingin anda urutkan"<<endl;
+    cin>>angka_awal;
+    cout<<"masukkan angka akhir dari angka yang ingin anda urutkan"<<endl;
+    cin>>angka_akhir;
+    cout<<"urutan angka yang akan muncul\n"<<endl;
+
+    if(angka_awal<angka_akhir){
+        for(int i=angka_awal;i<=angka_akhir;i++){
+            cout<<i<<endl;
+        }
+    }
+    
+        else if(angka_awal>angka_akhir){
+            for(int i=angka_awal;i>=angka_akhir;i--){
+                cout<<i<<endl;
+            }
+        }
+
+        else{
+            cout<<"mohon periksa lagi input anda"<<endl;
+        }
+    return 0;
+}
---
+++ Tipe_data.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int angka=20; //ini adalah integer//
+    string nama="Fariz";  //ini adalah string//
+    bool boolean=true;  //ini boolean//
+    double doublee=32.98798;  //ini double//
+    float floati=12.07;  //ini float
+}
