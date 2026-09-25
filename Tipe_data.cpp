0f74b70889ff0c418eb7ccbd9a3103d23b471c78
Fariz <rustamfariz022.gmail.com>
2026-09-25 23:55:08 +0800

    menambahkan tipe data char dan juga cara menghasilkan output tipe data

--- Tipe_data.cpp
+++ Tipe_data.cpp
@@ -5,6 +5,16 @@ int main(){
     int angka=20; //ini adalah integer//
     string nama="Fariz";  //ini adalah string//
     bool boolean=true;  //ini boolean//
-    double doublee=32.98798;  //ini double//
+    double doublee=65.34;  //ini double//
     float floati=12.07;  //ini float
+    char indeks='b';  //ini char
+
+    //cara memunculkan tipe data pada output
+    cout<<"angka:"<<angka<<endl;
+    cout<<"Nama:"<<nama<<endl;
+    cout<<"Kondisi:"<<boolean<<endl;
+    cout<<"Float:"<<floati<<endl;
+    cout<<"Indeks:"<<indeks<<endl;
+
+    return 0;
 }
--- looping_while.cpp
+++ looping_while.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main(){
-    cout<<"hello world"<<endl;
+    int i=0;
+    while(i<10){
+        cout<<i<<endl;
+        i++;
+        
+    }
     return 0;
 }
