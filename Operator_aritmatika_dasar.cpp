0682df3fbf6f2dc8cdfde328370fa0b44bbf8f58
Fariz <rustamfariz022@gmail.com>
2026-09-26 16:02:03 +0800

    menambahkan file baru untuk operator aritmatika penjumlahan

---
+++ Operator_aritmatika_dasar.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*penggunaan operator penjumlahan(+)*/
+    int a=4;
+    int b=9;
+    int hasil=a+b;
+    cout<<"operator penjumlahan"<<endl;
+    cout<<a<<"+"<<b<<"="<<hasil<<endl;   //hasilnya adalah 13//
+    return 0;
+
+}
